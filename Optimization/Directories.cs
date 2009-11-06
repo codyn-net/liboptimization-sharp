660f27033530bcec584c86ccb08d90352451e8c9
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2009-11-06 14:40:56 +0100

    Removed generated Directories.cs

--- Optimization/Directories.cs
+++
@@ -1,8 +0,0 @@
-namespace Optimization
-{
-	class Directories
-	{
-		public static string Prefix = "/home/jvanden/.packeteer/install";
-		public static string Lib = "/home/jvanden/.packeteer/install/lib";
-	}
-}
